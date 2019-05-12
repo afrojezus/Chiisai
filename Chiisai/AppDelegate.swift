d52396403a23543d001ea465f86cf8ad61ed5551
Thoralf Thelle <thoralf21@gmail.com>
2019-05-12 20:07:29 +0200

    Initial Commit

---
+++ Chiisai/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chiisai
+//
+//  Created by Thoralf Thelle on 12/05/2019.
+//  Copyright © 2019 thor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chiisai/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chiisai
+//
+//  Created by Thoralf Thelle on 12/05/2019.
+//  Copyright © 2019 thor. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
